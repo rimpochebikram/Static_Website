a287fe1386267a19bda62e0e9676ee97159404f6
rimpochebikram <rimpochebikram@gmail.com>
2026-07-17 14:32:29 +0545

    added insta and facebook

--- products.js
+++ products.js
@@ -9,21 +9,21 @@ const products = [
   {
     id: 2,
     name: "Singashan Kuber",
-    price: 8500,
+    price: 50000,
     category: "Statue",
     image: "./images/105859551_111979657235498_3989117061123004382_n.jpg"
   },
   {
     id: 3,
     name: "Aparamita",
-    price: 5000,
+    price: 30000,
     category: "Statue",
     image: "./images/106923655_123060389460758_5433697895689076579_n.jpg"
   },
   {
     id: 4,
     name: "Tara",
-    price: 12000,
+    price: 15000,
     category: "Statue",
     image: "./images/178412145_292085455891583_8629124554886026963_n.jpg"
   }
