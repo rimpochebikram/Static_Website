0cfa64a8aacac2292aa689a1b1e2d1367283a1ad
rimpochebikram <rimpochebikram@gmail.com>
2026-07-17 14:12:42 +0545

    Static website

---
+++ app.js
@@ -0,0 +1,91 @@
+const grid = document.getElementById("productGrid");
+const search = document.getElementById("search");
+
+let cart =
+    JSON.parse(localStorage.getItem("cart")) || [];
+
+updateCart();
+
+function renderProducts(data) {
+
+    grid.innerHTML = "";
+
+    data.forEach((p) => {
+
+        grid.innerHTML += `
+        <div class="product">
+
+            <img
+                src="${p.image}"
+                alt="${p.name}"
+                onerror="this.src='https://placehold.co/600x600?text=No+Image'"
+            >
+
+            <div class="product-body">
+
+                <h3>${p.name}</h3>
+
+                <div class="price">
+                    Rs. ${p.price.toLocaleString()}
+                </div>
+
+                <button onclick="addCart(${p.id})">
+                    <i class="fa fa-cart-plus"></i>
+                    Add To Cart
+                </button>
+
+                <a
+                    target="_blank"
+                    href="https://wa.me/9779840151586?text=${encodeURIComponent(
+                      `Hello, I am interested in ${p.name}`
+                    )}">
+                    <i class="fab fa-whatsapp"></i>
+                    WhatsApp Inquiry
+                </a>
+
+            </div>
+
+        </div>
+        `;
+    });
+}
+
+renderProducts(products);
+
+/* SEARCH */
+
+search.addEventListener("input", () => {
+
+    const q = search.value.toLowerCase();
+
+    const filtered = products.filter((p) =>
+        p.name.toLowerCase().includes(q)
+    );
+
+    renderProducts(filtered);
+});
+
+/* CART */
+
+function addCart(id) {
+
+    const product = products.find(
+        (p) => p.id === id
+    );
+
+    cart.push(product);
+
+    localStorage.setItem(
+        "cart",
+        JSON.stringify(cart)
+    );
+
+    updateCart();
+}
+
+function updateCart() {
+
+    document.getElementById(
+        "cartCount"
+    ).innerText = cart.length;
+}
---
+++ products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    name: "Buddha Statue",
+    price: 15000,
+    category: "Statue",
+    image: "./images/105571678_120865533013577_9208778171644514231_n.jpg"
+  },
+  {
+    id: 2,
+    name: "Singashan Kuber",
+    price: 8500,
+    category: "Statue",
+    image: "./images/105859551_111979657235498_3989117061123004382_n.jpg"
+  },
+  {
+    id: 3,
+    name: "Aparamita",
+    price: 5000,
+    category: "Statue",
+    image: "./images/106923655_123060389460758_5433697895689076579_n.jpg"
+  },
+  {
+    id: 4,
+    name: "Tara",
+    price: 12000,
+    category: "Statue",
+    image: "./images/178412145_292085455891583_8629124554886026963_n.jpg"
+  }
+];
